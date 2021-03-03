0a7ad087b0471a40d6051fa387e49e8bc4dd52bb
Gasque <75370906+LucasGasque@users.noreply.github.com>
2021-03-03 13:25:44 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,32 @@
-import tinkter
+n1 = float
+n2 = float
+funcao = ''
+ms = float
+ms_mais = float
+ms_menos = float
+resultado = float
+
+n1 = float(input('Digite um numero: '))
+funcao = input('Digite uma função \(+ - * /)')
+n2 = float(input('Digite um numero: '))
+
+if funcao == '+':
+    resultado = n1 + n2
+    print(resultado)
+elif funcao == '-':
+    resultado = n1 - n2
+    print(resultado)
+elif funcao == '*':
+    resultado = n1 * n2
+    print(resultado)
+elif funcao == '/':
+    resultado = n1 / n2
+    print(resultado)
+else:
+    print('Função invalida!')
+
+
+
+
+
+

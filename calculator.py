79939642b20721c4db6b6fae32167f27d32c4a32
Gasque <75370906+LucasGasque@users.noreply.github.com>
2021-03-03 13:53:24 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,24 +6,29 @@ ms_mais = float
 ms_menos = float
 resultado = float
 
-n1 = float(input('Digite um numero: '))
-funcao = input('Digite uma função \(+ - * /)')
-n2 = float(input('Digite um numero: '))
 
-if funcao == '+':
-    resultado = n1 + n2
-    print(resultado)
-elif funcao == '-':
-    resultado = n1 - n2
-    print(resultado)
-elif funcao == '*':
-    resultado = n1 * n2
-    print(resultado)
-elif funcao == '/':
-    resultado = n1 / n2
-    print(resultado)
-else:
-    print('Função invalida!')
+
+while funcao != 'off': 
+    n1 = float(input('Digite um numero: '))
+    funcao = input('Digite uma função \(+ - * /)')
+    n2 = float(input('Digite um numero: '))
+
+    if funcao == '+':
+        resultado = n1 + n2
+        print(resultado)
+    elif funcao == '-':
+        resultado = n1 - n2
+        print(resultado)
+    elif funcao == '*':
+        resultado = n1 * n2
+        print(resultado)
+    elif funcao == '/':
+        resultado = n1 / n2
+        print(resultado)
+    else:
+        print('Função invalida!')
+
+
 
 
 
